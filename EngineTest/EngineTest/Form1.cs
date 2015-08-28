6dca0f5993c411c1110a7c5a3f350bfc0b6111f0
hnerud <hnerud@applesauseinteractive.com>
2015-08-28 15:28:48 -0500

    this is the engine test that can run any engine

---
+++ EngineTest/EngineTest/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine;
+using System.Windows.Forms;
+using Tao.DevIl;
+using Tao.OpenGl;
+
+namespace EngineTest
+{
+    public partial class Form1 : Form
+    {
+        bool _fullscreen = false;
+        FastLoop _fastLoop;
+        StateSystem _system = new StateSystem();
+        Input _input = new Input();
+        TextureManager _textureManager = new TextureManager();
+       
+        
+        public Form1()
+        {
+            InitializeComponent();
+            simpleOpenGlControl1.InitializeContexts();
+
+            InitializeDisplay();
+            InitializeTextures();
+            InitializeGameState();
+            _fastLoop = new FastLoop(GameLoop);
+        }
+        private void InitializeGameState()
+        {
+        }
+
+        private void InitializeTextures()
+        {
+            // Init DevIl
+            Il.ilInit();
+            Ilu.iluInit();
+            Ilut.ilutInit();
+            Ilut.ilutRenderer(Ilut.ILUT_OPENGL);
+        }
+
+        private void UpdateInput()
+        {
+            System.Drawing.Point mousePos = Cursor.Position;
+            mousePos = simpleOpenGlControl1.PointToClient(mousePos);
+
+            // Now use our point definition, 
+            Engine.Point adjustedMousePoint = new Engine.Point();
+            adjustedMousePoint.X = (float)mousePos.X - ((float)ClientSize.Width / 2);
+            adjustedMousePoint.Y = ((float)ClientSize.Height / 2) - (float)mousePos.Y;
+            _input.MousePosition = adjustedMousePoint;
+            
+        }
+
+        private void GameLoop(double elapsedTime)
+        {
+            UpdateInput();
+            _system.Update(elapsedTime);
+            _system.Render();
+            simpleOpenGlControl1.Refresh();
+        }
+
+        private void InitializeDisplay()
+        {
+            if (_fullscreen)
+            {
+                FormBorderStyle = FormBorderStyle.None;
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                ClientSize = new Size(1280, 720);
+            }
+            Setup2DGraphics(ClientSize.Width, ClientSize.Height);
+        }
+        protected override void OnClientSizeChanged(EventArgs e)
+        {
+            base.OnClientSizeChanged(e);
+            Gl.glViewport(0, 0, this.ClientSize.Width, this.ClientSize.Height);
+            Setup2DGraphics(ClientSize.Width, ClientSize.Height);
+        }
+
+        private void Setup2DGraphics(double width, double height)
+        {
+            double halfWidth = width / 2;
+            double halfHeight = height / 2;
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Gl.glOrtho(-halfWidth, halfWidth, -halfHeight, halfHeight, -100, 100);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+        }
+    }
+
+}
